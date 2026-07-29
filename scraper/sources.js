818769c481b90092c34a2b930897f9220b4c374a
swapnilneharkar1 <swapnil.neharkar1@bajajfinserv.in>
2026-07-29 10:47:39 +0530

    Change htmlParse type for Business Standard source

--- scraper/sources.js
+++ scraper/sources.js
@@ -67,7 +67,7 @@ module.exports = {
         key: 'NEWSLETTER_3', label: 'Business Standard', cat: 'News',
         rss: 'https://www.business-standard.com/rss/latest.rss',
         src: 'https://www.business-standard.com/finance',
-        htmlParse: 'generic', headless: true, keywordFilter: BANK_NBFC_KEYWORDS,
+        htmlParse: 'bs_smallcard', headless: true, keywordFilter: BANK_NBFC_KEYWORDS,
       },
       {
         // Reuters killed its public RSS feeds in 2020, and the common "Google News RSS"
